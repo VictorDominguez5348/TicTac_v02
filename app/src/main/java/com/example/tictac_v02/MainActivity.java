da947a40de1d1f205f05b1fa5402ac57b37cee5c
VictorDominguez5348 <54280418+VictorDominguez5348@users.noreply.github.com>
2019-12-16 08:12:54 -0800

    Changing activity feature added

--- app/src/main/java/com/example/tictac_v02/MainActivity.java
+++ app/src/main/java/com/example/tictac_v02/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.tictac_v02;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+Button start_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nextPage = (Button) findViewById(R.id.startButton); //find the button by its assigned id
+
+//Make the button do something:
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        ticTacScreen.class);
+                startActivity(myIntent);
+
+            }
+        });//end of click listener
     }
-}
+    }
